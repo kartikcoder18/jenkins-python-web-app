f32bd3e26bf10af0e01bc6c8064244a4a9a9eecf
KARTIK KAISTHA <118113163+kartikcoder18@users.noreply.github.com>
2026-02-26 10:13:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
-AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 @app.route("/")
 def home():
     return "Hello from nnew Python Web App via Jenkins & Docker!"
