5450afe5d4ea988ec1b104046b4f4640ed913820
Ubuntu <ubuntu@ip-172-31-38-76.ap-south-1.compute.internal>
2026-03-03 04:26:27 +0000

    changed just app.py file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello newest ython Web App via Jenkins & Docker!"
+    return "Heo newest ython Web App via Jenkins & Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
